40468802ce660bafdde93b91d16a82c5b2649dcb
Cristian Gamboa <cristian.gamboa@codesa.com.co>
2024-02-09 21:51:10 -0500

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-test
+
+# App zonas afectadas
+
+This system allows for control and registration of coconut palm areas that are affected by different pests.
+
+
+## Deployment
+
+To deploy this project run
+
+```bash
+  docker-compose up -d
+```
+
+
+## Tech Stack
+
+**Client:** Angular
+
+**Server:** Java, SpringBoot
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/Cristian45/app_zones/tree/master
+```
+
+Backend
+
+Go to the project directory
+
+```bash
+  cd back
+```
+Start the server
+
+```bash
+  mvn
+```
+
+Frontend
+
+Go to the project directory
+
+```bash
+  cd back
+```
+Start the server
+
+```bash
+  mvn
+```
+
+## Screenshots
+
+![App Screenshot](https://github.com/Cristian45/app_zones/blob/master/diagrams/architecture.jpg)
+
+
+
+
+## Authors
+
+- [@Cristian45](https://github.com/Cristian45)
+
--- bd/01_insert.sql
+++ bd/01_insert.sql
@@ -1,5 +1,4 @@
 
----zonas
 INSERT INTO backzone.`zone`
 (id, name, description, palms_quantity, is_affected, createdat, updatedat)
 VALUES(16, 'zona A', 'descripción de zona a', 100, 'S', '2024-02-09 02:58:00', '2024-02-09 02:58:00');
@@ -10,7 +9,6 @@ INSERT INTO backzone.`zone`
 (id, name, description, palms_quantity, is_affected, createdat, updatedat)
 VALUES(18, 'zona c', 'descripción de zona c', 400, 'S', '2024-02-09 02:59:00', '2024-02-09 02:59:00');
 
----plagas
 INSERT INTO backzone.pest
 (id, name, description, createdat, updatedat)
 VALUES(12, 'Escama roja', 'Plaga presente en cítricos', '2024-02-09 03:00:00', '2024-02-09 03:00:00');
@@ -19,7 +17,6 @@ INSERT INTO backzone.pest
 VALUES(13, 'gualpa', 'Insecto conocido como el picudo', '2024-02-09 03:01:00', '2024-02-09 03:01:00');
 
 
---relacion zona-plagas
 INSERT INTO backzone.zone_pest
 (id, createdat, updatedat, zone_id_id, pest_id_id)
 VALUES(25, '2024-02-09 03:02:00', '2024-02-09 03:02:00', 16, 13);
